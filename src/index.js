3319d4ca72032fe4ea59d11a527e8868b802b71c
Marco Parra <mpdragon123@gmail.com>
2023-12-18 19:57:08 -0300

    Create Js Button

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function insertNumber(number) { //Faz os números aparecem na tela//
+    var number_plus = document.getElementById('results').innerHTML;
+    document.getElementById('results').innerHTML = number_plus + number
+}; //Cria uma váriavél chamada number_plus e adiciona a variavél number, 
+    //fazendo ambas aparecerem na calculadora//
+
+
+
